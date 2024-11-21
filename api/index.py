8674adddf3196d53717c25657f60925c1c835cd1
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-21 20:00:33 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+def text(t:str):
+    return t
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return text("him")
 
 @app.route('/about')
 def about():
