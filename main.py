02673233fa3c26d0d84bb20524d19545c597cd16
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-25 23:01:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-c=" 'Hello, everyone'\""
-#^INPUT HERE^
-p=-1;s=[];ts=[]
-while len(c)>p:
-  p+=1
-  if c[p]==" ":
-    s.append(c[p+1]);p+=1
-    if c[p+1]=="'":
-      p+=2
-      while c[p]!="'":ts.append(c[p]);p+=1
-      while len(ts)>0:ts.append(ts.pop(0)+ts.pop(0))
-  elif c[p]=="!":s.append("0" if s.pop()=="0" else "1")
-  elif c[p]=="\"":print("".join(s))
+print("Pascgolf")
+i=input("Input your code:\n");p=int(0)
+o=bool(0);e=bool(0)
+while len(i)>p and e!=1:
+   if i[p]=="s":o=not o;print(o)
+   p+=1
