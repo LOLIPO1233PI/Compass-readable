ef3436b7732ae17bd12775e0fb2bde2161324a8a
Gaham <glebsemenov1mail.ru@gmail.com>
2024-11-30 23:19:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ try:
          ts.reverse()
          while len(ts)>1:ts.append(ts.pop()+ts.pop())
          v["v"].append(ts.pop())
-      elif i[p+1]=="\""and i[p]==".":
-         p+=2
-         while i[p]!="\"":print(i[p],end="");p+=1
+      elif i[p]==".":
+         if i[p+1]=="\"":
+            p+=2
+            while i[p]!="\"":print(i[p],end="");p+=1
       elif i[p]!="["or"]"and o==0:e=1;print("command not in brackets")
       else:e=1;print("not a valid pascgolf command",i[p])
       p+=1
