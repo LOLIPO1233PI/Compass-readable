d3858522e632d6f7e5e40bd1df290c77140f8779
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-01 00:18:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ try:
          if i[p+1]=="\"":
             p+=2
             while i[p]!="\"":print(i[p],end="");p+=1
+         if i[p+1]=="v"and i[p+2]==":":
+            p+=3;ind=v["n"].index(i[p]);print(v["v"][ind])
       elif i[p]!="["or"]"and o==0:e=1;print("command not in brackets")
       else:e=1;print("not a valid pascgolf command",i[p])
       p+=1
