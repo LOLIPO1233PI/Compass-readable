bf9745144bee0e5cdce2cd04736de4829efcd314
Gaham <glebsemenov1mail.ru@gmail.com>
2025-02-11 01:23:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # A language based on Pascal
 class Compass:
  def phar(x:str):
+  oldx=x.strip().split("\n")
   x=x.strip().split("\n")
   p=po=0
   texts=[];funcs={}
@@ -10,8 +11,11 @@ class Compass:
     if x[p].startswith("begin "):
      po+=6
      while x[p][po]!=":":texts.append(x[p][po]);po+=1
-     x.insert(p+1,x[p][po+1::]);x[p]=x[p][6:po];funcs[x[p-1]]=x.pop(p+1)
+     x.insert(p+1,x[p][po+1::]);x[p]=x[p][6:po]
+     if not x[p+1]:print(f"ERROR:Function {x[p]} has no body");exit()
+     else:funcs[x[p-1]]=x.pop(p+1)
     po+=1
    p+=1
+   if"main"not in x:print(f"\"{oldx[0]}\"\nERROR:Function main requiers to be initated");exit()
   print(x,funcs)
-Compass.phar("""begin main:print("Hello, world!")""")
+Compass.phar("""""")
