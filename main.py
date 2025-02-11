ec035d13f8c6630086525b913beac836ca960d05
Gaham <glebsemenov1mail.ru@gmail.com>
2025-02-12 00:19:36 +0300

    added some stuff

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # Compass implementation by Gaham (Thevitebsk) Y25
 # A language based on Pascal
-class Compass:
- def phar(x:str):
-  oldx=x.strip().split("\n")
-  x=x.strip().split("\n")
-  p=po=0;texts=[];funcs={}
+def Compass(x):
+ def phar(x):
+  p=po=0
+  x=x.strip().split("\n");oldx=x
+  texts=[];funcs={}
   while len(x)>p:
    while len(x[p])>po:
     if x[p].startswith("begin "):
@@ -13,11 +13,23 @@ class Compass:
      x.insert(p+1,x[p][po+1::]);x[p]=x[p][6:po]
      if not x[p+1]:print(f"ERROR:Function {x[p]} has no body");exit()
      else:funcs[x[p-1]]=x.pop(p+1)
-    elif x[p][po]==";":break
     po+=1
    po=0
    p+=1
    if"main"not in x:print(f"\"{oldx[0]}\"\nERROR:Function main requiers to be initiated");exit()
    else:x.append(funcs["main"]);x.pop(x.index("main"));break
-  print(x,funcs)
-Compass.phar("""""")
+  return [x,funcs]
+ def execu(x):
+  p=po=0
+  c=x[0];funcs=x[1];
+  print(c,funcs)
+  while len(c)>p:
+   while len(c[p])>po:
+    if c[p].startswith("text("):
+     po+=5
+     while c[p][po]!=")":print(c[p][po],end="");po+=1
+    po+=1
+   po=0
+   p+=1
+ execu(phar(x))
+Compass("""""")
