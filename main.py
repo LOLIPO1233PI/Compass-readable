9538597079d5074a062326b84ce322a5b1f7ff01
Gaham <glebsemenov1mail.ru@gmail.com>
2025-02-14 01:31:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ def Compass(x):
   else:x.append(funcs["main"]);x.pop(x.index("main"))
   return [x,funcs]
  def execu(x):
-  p=po=0
-  c=x[0];funcs=x[1]
+  p=po=0 ; c=x[0];funcs=x[1]
   while len(c)>p:
    while len(c[p])>po:
     if c[p].startswith("text("):
