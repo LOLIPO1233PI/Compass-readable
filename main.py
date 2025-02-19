44b09c6f5feb2a98675dc710593f971f40699a1f
Gaham <glebsemenov1mail.ru@gmail.com>
2025-02-20 01:54:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Compass implementation by Gaham (Thevitebsk) Y25
 # A language based on Pascal
-import time
-def Compass(x)->...:
- t=time.time()
- def phar(x)->list:
+from time import*
+def Compass(x):
+ var={};t=time()
+ def phar(x):
   p=po=0
   x=x.strip().split("\n");oldx=x
   texts=[];funcs={}
@@ -11,35 +11,25 @@ def Compass(x)->...:
    while len(x[p])>po:
     if x[p].startswith("begin "):
      po+=6
-     while x[p][po]!=":":
-      texts.append(x[p][po]);po+=1
-     x.insert(p+1,x[p][po+1::])
-     x[p]=x[p][6:po]
-     if not x[p+1]:
-      print(f"ERROR:Function {x[p]} has no body");exit()
-     else:
-      funcs[x[p-1]]=x.pop(p+1)
+     while x[p][po]!=":":texts.append(x[p][po]);po+=1
+     x.insert(p+1,x[p][po+1::]);x[p]=x[p][6:po]
+     if not x[p+1]:print(f"ERROR:Function {x[p]} has no body");exit()
+     else:funcs[x[p-1]]=x.pop(p+1)
+    elif"::"in x[p]:print(x[p])
     po+=1
    po=0
    p+=1
-  if"main"not in funcs:
-    print(f"\"{oldx[0]}\"\nERROR:Function main requiers to be initiated");exit()
-  else:
-    x.append(funcs["main"])
-    x.pop(x.index("main"));funcs.pop("main")
+  if"main"in funcs:x.append(funcs["main"]);x.pop(x.index("main"))
   return [x,funcs]
- def execu(x)->...:
-  p=po=0 ; c=x[0]#;funcs=x[1]
+ def execu(x):
+  p=po=0
+  c=x[0];funcs=x[1]
   while len(c)>p:
    while len(c[p])>po:
     if c[p].startswith("text("):
      po+=5
-     while c[p][po]!=")":
-      print(c[p][po],end="");po+=1
-    else:
-      print(f"\"{c[p]}\"\nERROR:Unknown command");exit()
+     while c[p][po]!=")":print(c[p][po],end="");po+=1
     po+=1
    po=0
    p+=1
- execu(phar(x))
- print(f"\ntook {time.time()-t:.4f} seconds")
+ execu(phar(x));print(f"\ntook {time()-t:.4f} seconds")
