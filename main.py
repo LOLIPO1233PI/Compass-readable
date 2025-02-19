f16ad4c737e85f2d00c3521d5124a2e6017ef6fd
Gaham <glebsemenov1mail.ru@gmail.com>
2025-02-20 02:17:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,15 +15,18 @@ def Compass(x):
      x.insert(p+1,x[p][po+1::]);x[p]=x[p][6:po]
      if not x[p+1]:print(f"ERROR:Function {x[p]} has no body");exit()
      else:funcs[x[p-1]]=x.pop(p+1)
-    elif"::"in x[p]:print(x[p])
+    elif x[p][po]==":":
+     while 0<po:po-=1
+     while x[p][po]!=";":texts.append(x[p][po]);po+=1
+     var["".join(texts[0:texts.index(":")])]="".join(texts[texts.index(":")+1::])
     po+=1
    po=0
    p+=1
   if"main"in funcs:x.append(funcs["main"]);x.pop(x.index("main"))
-  return [x,funcs]
+  return [x,funcs,var]
  def execu(x):
   p=po=0
-  c=x[0];funcs=x[1]
+  c=x[0];funcs=x[1];var=x[2]
   while len(c)>p:
    while len(c[p])>po:
     if c[p].startswith("text("):
