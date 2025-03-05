5e473f89b30a0a540f2d7eed2110276e00ceb7c1
LOLIPO1233PI <gttyeio@gmail.com>
2025-03-05 13:29:33 +0000

    small update
    this update enhances the code readability for a more easy contribution

--- manual.py
+++ manual.py
@@ -1,13 +1,28 @@
 def manual():
-  print("Compass is a programming language that is designed to be based on Pascal.","The language is not finished. yet!","="*55,
-       "func:","sytax structure: \"func name:body\"","function definition command\n",
-       "print:","syntax structure: \"print(content1,...,contentN)","prints content of a variable or a string",sep="\n")
+    print(
+        "Compass is a programming language that is designed to be based on Pascal.",
+        "The language is not finished. yet!",
+        "=" * 55,
+        "func:",
+        'sytax structure: "func name:body"',
+        "function definition command\n",
+        "print:",
+        'syntax structure: "print(content1,...,contentN)',
+        "prints content of a variable or a string",
+        sep="\n",
+    )
+
+
 def cpecm():
-  print(f"Compass Pharsing and Execution Console Manual","="*55,
-        "\"$help\" Print's the manual for Compass",
-        "\"$clear\" Clears the memory (if any)",
-        "\"$exec\" Executes the memory as code",
-        "\"$exit\" End's the CPEC session",
-        "\"$modify\" Modifies a line in the memory (if any)",
-        "\"$list\" Prints the memory",
-        "\"$cpec\" Prints the manual for CPEC",sep="\n")
+    print(
+        "Compass Pharsing and Execution Console Manual",
+        "=" * 55,
+        '"$help" Print\'s the manual for Compass',
+        '"$clear" Clears the memory (if any)',
+        '"$exec" Executes the memory as code',
+        '"$exit" End\'s the CPEC session',
+        '"$modify" Modifies a line in the memory (if any)',
+        '"$list" Prints the memory',
+        '"$cpec" Prints the manual for CPEC',
+        sep="\n",
+    )
